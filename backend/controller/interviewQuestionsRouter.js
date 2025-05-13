54085146b6c89dccb6f822fb89e606952112cea8
Sahithi-630 <bhumireddysahithi@gmail.com>
2025-05-13 17:46:25 +0530

    Test

--- backend/controller/interviewQuestionsRouter.js
+++ backend/controller/interviewQuestionsRouter.js
@@ -1,19 +1,7 @@
 const express = require('express');
 const interviewQuestionRouter = express.Router();
 
-interviewQuestionRouter.get('/interviewQuestion', async (req, res) => {
-    try {
-        const interviewQuestion = [
-            { Question: 'What is the full form of HTML?', answer: 'Hypertext Markup Language' },
-            { Question: 'What is the full form of CSS?', answer: 'Cascading Style Sheets' }
-        ];
 
-        res.status(200).json(interviewQuestion); 
-    } catch (error) {
-        console.error('Error fetching questions:', error);
-        res.status(500).json({ message: 'Internal Server Error' }); 
-    }
-});
 
 interviewQuestionRouter.put("/updateIQuestion/:id",async(req,res)=>{
     try {
--- backend/controller/jobBoardRouter.js
+++ backend/controller/jobBoardRouter.js
@@ -1,19 +1,7 @@
 const express = require('express');
 const jobBoardRouter = express.Router();
 
-jobBoardRouter.get('/jobBoard', async (req, res) => {
-    try {
-        const jobBoard = [
-            {role:'Software Engineer',company:'infosys', Salary:'6lakhs', place:'Hyderabad' },
-            {role:'Software Engineer',company:'infosys', Salary:'6lakhs', place:'Hyderabad'}
-        ];
 
-        res.status(200).json(jobBoard); 
-    } catch (error) {
-        console.error('Error fetching questions:', error);
-        res.status(500).json({ message: 'Internal Server Error' }); 
-    }
-});
 jobBoardRouter.put("/updateJobBoard/:id",async(req,res)=>{
     try {
         const{id}=req.params;
