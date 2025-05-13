3f72e1da83557dac9f8558ca9673eb65a12ffcdb
Sahithi-630 <bhumireddysahithi@gmail.com>
2025-05-13 17:50:44 +0530

    Get

--- backend/controller/companyWiseQuestionsRouter.js
+++ backend/controller/companyWiseQuestionsRouter.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const companyWiseQuestionRouter = express.Router();
 
+companyWiseQuestionRouter.get('/companyWiseQuestion', async (req, res) => {
+    try {
+        const companyWiseQuestion = [
+            { Question: 'What is the full form of HTML?', answer: 'Hypertext Markup Language' },
+            { Question: 'What is the full form of CSS?', answer: 'Cascading Style Sheets' }
+        ];
 
+        res.status(200).send(companyWiseQuestion); 
+    } catch (error) {
+        console.error('Error fetching questions:', error);
+        res.status(500).send({ message: 'Internal Server Error' }); 
+    }
+});
 
 companyWiseQuestionRouter.put("/updateQuestion/:id",async(req,res)=>{
     try {
