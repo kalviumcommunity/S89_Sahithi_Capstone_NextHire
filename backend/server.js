27660b4630d42fd2f8d017106ee2aeba59661185
Sahithi-630 <bhumireddysahithi@gmail.com>
2025-05-14 10:19:44 +0530

    tst

--- backend/server.js
+++ backend/server.js
@@ -1,43 +1,29 @@
 const express = require("express");
 const app = express();
-const cors = require('cors');
-
-const CompanyWiseQuestion = require('C:/Users/user/Documents/Capstone Project/S89_Sahithi_Capstone_NextHire/backend/models/companyWiseQuestionsSchema.js');
-
 
 const mongoose = require("mongoose");
 
 const dotenv = require("dotenv");
 dotenv.config();
 
-app.use(cors());
-
 app.use(express.json());
-mongoose.connect('mongodb+srv://bhumireddysahithi:IlQ8h2lSJnS1uyw5@cluster0.ao7krsf.mongodb.net/',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
 
-//const interviewQuestionsRouter = require("./controller/interviewQuestionsRouter");
-//const jobBoardRouter = require("./controller/jobBoardRouter");
-//const companyWiseQuestionRouter = require("./controller/companyWiseQuestionsRouter");
+const interviewQuestionsRouter = require("./controller/interviewQuestionsRouter");
+const jobBoardRouter = require("./controller/jobBoardRouter");
+const companyWiseQuestionRouter = require("./controller/companyWiseQuestionsRouter");
 
 
-//app.use("/interviewQuestion", interviewQuestionsRouter);
-//app.use("/jobBoard",jobBoardRouter);
-//app.use("/companyWiseQuestion",companyWiseQuestionRouter);
-
-app.get('/companyWiseQuestion', async(req, res) => {
-    const companyWiseQuestion = await CompanyWiseQuestion.find();
-    res.json(companyWiseQuestion);
-});
+app.use("/api", interviewQuestionsRouter);
+app.use("/jobBoard",jobBoardRouter);
+app.use("/companyWiseQuestion",companyWiseQuestionRouter);
 
 
 
-app.listen(3000,async()=>{
+const PORT = 3000; // Or any port you prefer
+app.listen(PORT,async()=>{
     try {
-        //await mongoose.connect(process.env.MONGO)
-        console.log("Server running on port 3000");
+        await mongoose.connect(process.env.MONGO)
+        console.log(`Server is running on http://localhost:${PORT}`);
     } catch (error) {
         console.log("Error",error)
     }
